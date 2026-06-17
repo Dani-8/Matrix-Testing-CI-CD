5bab3e434d0ce9400fa8cff7bfbec07e9aa388d0
Dani-8 <daniyalw6190@gmail.com>
2026-06-18 01:01:08 +0500

    Updated

--- backend/server.test.js
+++ backend/server.test.js
@@ -1,3 +1,14 @@
+const request = require('supertest')
+const app = require('./server')
+
+describe("Get API Endpoint", () => {
+    it("should return a message", async () => {
+        const res = await request(app).get("/")
+
+        expect(res.statusCode).toEqual(200)
+        expect(res.headers['content-type']).toMatch(/text\/html/)
+    })
+})
 
 
 
