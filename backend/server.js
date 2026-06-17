1e425d79b6e96c2f75cddaca33acc68447b4dfc9
Dani-8 <daniyalw6190@gmail.com>
2026-06-18 00:50:59 +0500

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+// ============================================
+// ============================================
+
+app.get("/", (req, res) => {
+    res.send("Hello, This is the Matrix Testing (CI/CD) Server!")
+})
 
 
 
+// ============================================
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
